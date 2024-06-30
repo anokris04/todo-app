e353c61a90938a8fa4f66dafae68042210a60970
Anoop Rastogi <krishnaanoop03.ak@gmail.com>
2024-06-30 17:13:52 +0530

    latest responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import Navbar from './components/Navbar';
 import Calendar from './components/Calendar';
 import Todolist from './components/Todolist';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isNavVisible, setNavVisible] = useState(false);
 
   return (
-    <div className='grid grid-cols-6 h-full'>
-      <div className='col-span-1 bg-primary'>
-        <Navbar/>
+    <div className="h-full grid grid-cols-12">
+      {/* Mobile Navbar */}
+      <div className="md:hidden flex items-center justify-between bg-primary p-4 col-span-12">
+        <FaBars onClick={() => setNavVisible(!isNavVisible)} className="cursor-pointer text-white" />
+        <div className="text-white cursor-pointer" onClick={() => setNavVisible(false)}>TaskME</div>
       </div>
-      <div className='col-span-5 bg-middle'>
-        <Todolist/>
+
+      {/* Sidebar */}
+      <div className={`fixed inset-0 bg-primary z-50 transform ${isNavVisible ? 'translate-x-0' : '-translate-x-full'} transition-transform md:static md:translate-x-0 md:col-span-1 col-span-12 md:block hidden`}>
+        <Navbar />
+      </div>
+
+      {/* Main Content */}
+      <div className="col-span-12 md:col-span-8 bg-middle p-4">
+        <Todolist />
+      </div>
+
+      {/* Right Column */}
+      <div className="col-span-12 md:col-span-3 bg-secondary p-4 hidden md:flex md:flex-col">
+        <Calendar />
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="flex flex-col mx-2 my-4 items-center justify-between h-full">
         <div className="text-white">
           <div className="text-2xl font-bold">Task<span className="font-normal">ME</span></div>
-          <div className="text-md">Your very own<br />Task Planner</div>
+          
           <ul className="flex flex-col mt-10 gap-8 text-xl text-white font-semibold">
             <li className="hovering">Home</li>
             <li className="hovering">About</li>
